240c8706f60f38fd4213d93ebdec1a3898b47415
lfld287@gmail.com <lfld287@gmail.com>
2021-06-10 16:06:27 +0800

    测试脚本，无话可说

--- test_newthing.py
+++ test_newthing.py
@@ -1,38 +1,43 @@
 from PIL.Image import blend
 import cv2
 import numpy as np
+import myImage.sift
 
 #打开一张图片
 img = cv2.imread("tmp.png")
+temp = cv2.imread("resource/arkNights/main/feature/offer.png")
 
-img_gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 
-# cv2.imshow("123",img_gray)
+myImage.sift.drawMatchRect(img,temp)
 
-img_blur = cv2.blur(img_gray,ksize=(3,3))
+# img_gray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 
-# adaptiveThreshold
+# # cv2.imshow("123",img_gray)
 
-img_out=cv2.adaptiveThreshold(img_blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,5,5)
+# img_blur = cv2.blur(img_gray,ksize=(3,3))
 
-sobel_x = cv2.Sobel(img_blur, cv2.CV_64F, dx = 1, dy = 0, ksize = 5)
-sobel_y = cv2.Sobel(img_blur, cv2.CV_64F, dx = 0, dy = 1, ksize = 5)
-blended = cv2.addWeighted(src1=sobel_x, alpha=0.5, src2=sobel_y,
-                          beta=0.5, gamma=0)
-laplacian = cv2.Laplacian(img_blur, cv2.CV_64F)
+# # adaptiveThreshold
 
-# cv2.imshow("123",img_out)
+# img_out=cv2.adaptiveThreshold(img_blur,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,5,5)
 
-k=cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+# sobel_x = cv2.Sobel(img_blur, cv2.CV_64F, dx = 1, dy = 0, ksize = 5)
+# sobel_y = cv2.Sobel(img_blur, cv2.CV_64F, dx = 0, dy = 1, ksize = 5)
+# blended = cv2.addWeighted(src1=sobel_x, alpha=0.5, src2=sobel_y,
+#                           beta=0.5, gamma=0)
+# laplacian = cv2.Laplacian(img_blur, cv2.CV_64F)
 
-e=cv2.erode(img_gray,k)
+# # cv2.imshow("123",img_out)
 
-#canny edge
+# k=cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
 
-img_canny = cv2.Canny(img_blur,100,200)
+# e=cv2.erode(img_gray,k)
 
-cv2.xfeatures2d.SURF_create()
+# #canny edge
 
-cv2.imwrite("img_at.png",img_canny)
+# img_canny = cv2.Canny(img_blur,100,200)
+
+# cv2.xfeatures2d.SURF_create()
+
+# cv2.imwrite("img_at.png",img_canny)
 
 
