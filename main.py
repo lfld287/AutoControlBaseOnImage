93236ef4b3cef2bef8da961c0d9593efea4a83b1
lfld287@gmail.com <lfld287@gmail.com>
2021-06-18 17:08:46 +0800

    更新日志心得

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import android.arkNights as MRFZ
 import android.screenRotation as sr
 import time
 
+'''
+模板匹配 速度快 分辨率一定情况下 准确率高， 还是首选
+各种基于特征的匹配方法，由于人工ui有很多规则的图像 大量相同的配色，还有印刷体文字，都使得基于特征又慢又不准确
+后面还是想办法基于模板匹配进行开发。
+'''
+
+
+
 d = u2.connect()
-print(d.app_current())
-print(d.info)
-session = MRFZ.arkNights(d,True)
+
+session = MRFZ.arkNights(d,False)
 session.checkAndStart()
-session.gotoMainMenu()
+session.gotoFight_1_7()
+
